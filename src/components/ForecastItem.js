16b99ad47c7bad9d53c07721639a98a76baf7719
Kavya Madhu <kavya.m@sigmainfo.net>
2021-05-11 17:32:58 +0530

    corrected earning

--- src/components/ForecastItem.js
+++ src/components/ForecastItem.js
@@ -4,7 +4,7 @@ import '../stylesheets/ForecastItem.css'
 import { ForecastDetailHeader } from './ForecastDetailHeader'
 function ForecastItem(props) {
     // Displaying the info of individual forecast as a bootstrap card
-    const { name, title } = props.item
+    const { title } = props.item
     return (
         <div>
             <Card className='forecast' onClick={props.onClick}>
